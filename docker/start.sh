44553340a52d18446f10a4dc86e4ea89813323c4
robby-dermody <robbyd@u20.org>
2016-06-15 14:43:28 -0400

    simplify start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -7,15 +7,5 @@ if [ ! -d /armory-utxsvr/armory_utxsvr.egg-info ]; then
     cd /armory-utxsvr; python2 setup.py develop; cd /
 fi
 
-# Specify defaults (defaults are overridden if defined in the environment)
-EXTRA_PARAMS=""
-DEFAULT_BITCOIN_URL="http://rpc:rpc@bitcoin:8332"
-if [ -n "$TESTNET" ]; then
-    EXTRA_PARAMS="${EXTRA_PARAMS} --testnet"
-    DEFAULT_BITCOIN_URL="http://rpc:rpc@bitcoin-testnet:18332"
-fi
-
-: ${BITCOIND_URL:=$DEFAULT_BITCOIN_URL}
-
 # bitcoind_url must be passed in as first argument
-BITCOIND_PATH=${BITCOIND_PATH} DISPLAY=localhost:1.0 xvfb-run --auto-servernum /usr/local/bin/armory-utxsvr ${EXTRA_PARAMS} $BITCOIND_URL
+BITCOIND_PATH=${BITCOIND_PATH} DISPLAY=localhost:1.0 xvfb-run --auto-servernum /usr/local/bin/armory-utxsvr ${PARAMS} $BITCOIND_URL
