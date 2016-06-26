129bbf63b74624dd86ee9fcf5197cf62b3f214ab
robby-dermody <robbyd@u20.org>
2016-06-26 10:53:33 -0400

    update start.sh signal handling

--- docker/start.sh
+++ docker/start.sh
@@ -8,5 +8,13 @@ if [ ! -d /armory-utxsvr/armory_utxsvr.egg-info ]; then
     cd /armory-utxsvr; python2 setup.py develop; cd /
 fi
 
-# bitcoind_url must be passed in as first argument
-BITCOIND_PATH=${BITCOIND_PATH} RPC_HOST=${RPC_HOST} DISPLAY=localhost:1.0 xvfb-run --auto-servernum /usr/local/bin/armory-utxsvr ${PARAMS} $BITCOIND_URL
+# Launch, utilizing the SIGTERM/SIGINT propagation pattern from
+# http://veithen.github.io/2014/11/16/sigterm-propagation.html
+: ${PARAMS:=""}
+trap 'kill -TERM $PID' TERM INT
+BITCOIND_PATH=${BITCOIND_PATH} RPC_HOST=${RPC_HOST} DISPLAY=localhost:1.0 xvfb-run --auto-servernum /usr/local/bin/armory-utxsvr ${PARAMS} $BITCOIND_URL &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
